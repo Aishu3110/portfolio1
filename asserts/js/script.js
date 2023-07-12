cdf5bc6305637069962b2d8be8f494c837c38d6a
Aishu3110 <aishwarya@pyramidions.in>
2023-07-12 15:35:28 +0530

    folder structure modified

--- asserts/js/script.js
+++ asserts/js/script.js
@@ -1,12 +1,13 @@
 window.addEventListener("scroll",function(e){
     if(this.window.scrollY>4)
     {
-        
+    console.log("working");  
      let header=document.getElementById('navigation')
-navigation.className="sticky";
+navigation.className="navigation";
     }
     else{
         let header=document.getElementById('navigation')
-navigation.className="navigation";
+navigation.className="sticky";
     }
 })
+
